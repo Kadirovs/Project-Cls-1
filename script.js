731fcdaac153848a1375e9e4de37a40e0b912416
Kadirov Kamoliddin <kadirovkamoliddin24@gmail.com>
2024-10-10 20:33:52 +0400

    Add all files

---
+++ script.js
@@ -0,0 +1,42 @@
+function imgred(){
+    document.getElementById('imgblack').src='image21.jpeg'
+}
+function imgblack(){
+    document.getElementById('imgblack').src='image22.jpeg'
+}
+function imggray(){
+    document.getElementById('imgblack').src='image24.jpeg'
+}
+
+function imggred(){
+    document.getElementById('imggblack').src='image21.jpeg'
+}
+function imggblack(){
+    document.getElementById('imggblack').src='image23.jpeg'
+}
+function imgggray(){
+    document.getElementById('imggblack').src='image19.jpeg'
+}
+
+function imgggred(){
+    document.getElementById('imgggblack').src='image21.jpeg'
+}
+function imgggblack(){
+    document.getElementById('imgggblack').src='image15.jpeg'
+}
+function imggggray(){
+    document.getElementById('imgggblack').src='image14.jpeg'
+}
+
+function imggggred(){
+    document.getElementById('imggggblack').src='image21.jpeg'
+}
+function imggggblack(){
+    document.getElementById('imggggblack').src='image12.jpeg'
+}
+function imgggggray(){
+    document.getElementById('imggggblack').src='image10.jpeg'
+}
+
+
+
